6c76fe1fb8c75fbc078728f0e759bb30020006a2
DESKTOP-8G1QLH1\tey_p <panuwit.inta@bumail.net>
2021-01-29 21:31:01 +0700

    Send Assignment Week1

---
+++ New Unity Project/Assets/search01.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class search01 : MonoBehaviour
+{
+    private string profile;
+    public Text found;
+    public InputField file;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        profile = file.text;
+    }
+
+    void isFound()
+    {
+        found.text = profile + " is found";
+
+
+    }
+    public void Button()
+    {
+        switch (profile)
+        {
+                case "Minecraft":
+                isFound();
+                break;
+
+                case "GoldenApple":
+                isFound();
+                break;
+
+                case "Slime":
+                    isFound();
+                    break;
+
+                case "Axe":
+                    isFound();
+                    break;
+
+                case "Sword":
+                 isFound();
+                break;
+            default:
+                found.text = profile + " is not found";
+                break;
+            }
+    }
+}
